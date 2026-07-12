47d2ef33e46eeb18064238f0fa15c73cd4cfdd14
Коля <styxk96@gmail.com>
2026-07-12 21:56:06 +0300

    Доработка + использование компонента кнопки

--- frontend/app/not-found.js
+++ frontend/app/not-found.js
@@ -1,17 +1,19 @@
-import Link from "next/link";
 import Image from "next/image";
+import Button from "@/shared/components/Button/Button";
 
 export default function NotFound() {
   return (
-    <div className="relative flex flex-col items-center md:items-start text-center md:text-left gap-[40px] pt-[40px] px-[16px] pb-0 md:p-[40px] lg:p-15 bg-black mt-12 mb-20 rounded-4xl">
-      <div className="flex flex-col items-center text-center md:items-start md:text-left md:w-1/2 gap-4">
-        <p className="text-h1 text-green">404</p>
-        <p className="text-h2 text-white">Похоже, урок пошёл не по расписанию</p>
-        <p className="text-2 text-gray">Страница не найдена или была перемещена. Вернитесь на главную и продолжите обучение.</p>
-        {/* TODO: Сделать компонент для кнопок */}
-        <Link href="/" className="text-2 underline">
-          Вернуться на главную
-        </Link>
+    <div className="relative flex flex-col items-center md:items-start text-center md:text-left gap-[40px] pt-[40px] px-[16px] pb-0 md:p-[40px] lg:p-15 min-[1920px]:px-[120px]! min-[1920px]:py-[80px]! bg-black mt-12 mb-20 rounded-4xl">
+      <div className="flex flex-col items-center text-center md:items-start md:text-left md:w-1/2">
+        <p className="text-h1 text-green pb-2">404</p>
+        <p className="text-h2 text-white pb-3 lg:pb-5">Похоже, урок пошёл не по расписанию</p>
+        <p className="text-2 text-gray pb-10">Страница не найдена или была перемещена. Вернитесь на главную и продолжите обучение.</p>
+        <Button 
+          href="/" 
+          label="Вернуться на главную" 
+          width="294px" 
+          labelClassName="text-button"
+        />
       </div>
 
       <Image
