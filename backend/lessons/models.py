c222669320baadddf98b3e7ac71b84d08357cbae
vi.vala.vi <sovestyaaaa@gmail.com>
2026-09-26 22:53:17 +0300

    change models fields & add is_active=Fase user default

--- backend/lessons/models.py
+++ backend/lessons/models.py
@@ -386,7 +386,7 @@ class TestEssayAiElement(models.Model):
         help_text="От 1 до 32 767",
         verbose_name="Максимальное количество баллов",
     )
-    mention_things = models.CharField(max_length=500, verbose_name="Что должен упомянуть(влияет на оценку)")
+    mention_things = models.TextField(verbose_name="Что должен упомянуть(влияет на оценку)")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
 
